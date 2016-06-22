f33ce1bd60dbbb8482f43afbf5d687b094c84461
Karamaz0V1 <florent.guiotte@gmail.com>
2016-06-22 10:46:07 +0200

    Linux workaround

--- TextRPG/Classes/main.cpp
+++ TextRPG/Classes/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <conio.h>
+//#include <conio.h>
 #include <random>
 #include <chrono>
 #include <thread>
@@ -9,6 +9,57 @@
 #include "Warrior.h"
 #include "Wizard.h"
 #include "Robber.h"
+
+// *** Linux Workaround ***
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, new_g;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new_g = old; /* make new settings same as old settings */
+  new_g.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new_g.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new_g); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+
+/* TNTAntoine specific workaround */
+char _getch(void) 
+{
+  return getch_(0);
+}
+// *** End of workaround ***
 
 
 using namespace std;
